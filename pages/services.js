5641b94304f83478ba95abf3ce154584b0b86fcd
Elinyamangeni <222176504@edu.vut.ac.za>
2025-09-20 15:55:03 +0200

    Add Next.js starter files for Photocopy Shop website

---
+++ pages/services.js
@@ -0,0 +1,21 @@
+import Layout from '../components/Layout';
+
+export default function Services() {
+  return (
+    <Layout>
+      <h2>Our Services</h2>
+      <ul>
+        <li>Typing Documents</li>
+        <li>Printing Documents</li>
+        <li>Laminating Documents</li>
+        <li>Scanning Documents</li>
+        <li>Photocopying Documents</li>
+        <li>Fax Services</li>
+        <li>Telephone Services</li>
+      </ul>
+      <p>
+        For inquiries about specific services or bulk orders, please <a href="/contact">contact us</a>.
+      </p>
+    </Layout>
+  );
+}
